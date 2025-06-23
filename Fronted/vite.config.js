28fc8b0e3189fce5b4e50ef2a7c0620affa618e0
albrightraj2021 <albrightraj1211@gmail.com>
2025-06-23 16:26:33 +0530

    Update sitemap.xml with new last modified dates; remove unused pagesSitemap plugin from vite.config.js

--- Fronted/vite.config.js
+++ Fronted/vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pagesSitemap from 'vite-plugin-pages-sitemap';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    pagesSitemap({
-      hostname: 'https://thina-blog.vercel.app',
-      routes: [
-        '/',
-        '/blog',
-      ],
-    }),
-  ],
+    tailwindcss(),],
 });
