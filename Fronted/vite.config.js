c201409e55162dab5a5e4aaf0b873775cb1927a9
albrightraj2021 <albrightraj1211@gmail.com>
2025-06-23 15:26:17 +0530

    Update sitemap.xml with new last modified dates; remove unused createHtmlPlugin import from vite.config.js

--- Fronted/vite.config.js
+++ Fronted/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import pagesSitemap from 'vite-plugin-pages-sitemap';
 import tailwindcss from '@tailwindcss/vite'
 
